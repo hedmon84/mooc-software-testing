56e9137635ac2fff2d4c5864dacddd70b51cc7e0
hedmon84 <hedmonlopes@gmail.com>
2021-07-27 11:50:07 -0600

    Testing java

--- src/test/java/tudelft/gettingstarted/GettingStartedTest.java
+++ src/test/java/tudelft/gettingstarted/GettingStartedTest.java
@@ -19,7 +19,7 @@ public class GettingStartedTest {
         Assertions.assertEquals(5, result);
     }
 
-   @Test
+    @Test
     public void addFiveToMinus20() {
         int result = new GettingStarted().addFive(-20);
         Assertions.assertEquals(-15,result);
